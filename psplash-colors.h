bfe722b013927d039929d68a3c2d86b2b74fa4b1
Ryan Coe <nismoryco@icloud.com>
2014-01-17 18:53:46 -0800

    change background color to black

--- psplash-colors.h
+++ psplash-colors.h
@@ -20,7 +20,7 @@
 #define _HAVE_PSPLASH_COLORS_H
 
 /* This is the overall background color */
-#define PSPLASH_BACKGROUND_COLOR 0xec,0xec,0xe1
+#define PSPLASH_BACKGROUND_COLOR 0,0,0
 
 /* This is the color of any text output */
 #define PSPLASH_TEXT_COLOR 0x6d,0x6d,0x70
